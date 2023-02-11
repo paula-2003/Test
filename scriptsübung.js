8cbeb02760c92679eac8491cf1392aaba8ac953b
paula-2003 <Paula.Gerhardt@stud.hs-coburg.de>
2023-02-11 13:58:23 +0100

    deleted unnecessary files

--- scriptsübung.js
+++
@@ -1,9 +0,0 @@
-const questionList = ["Wer ist früher geboren? Isaac Newton oder Paula Gerhardt", "Wer ist Adolf Hitler?", "Wer ist der größte Youtuber?", "Welche Partei ist scheiße?", "Wer ist in diesem Raum am schlausten- Ironie?"]
-
-const question = document.getElementById("question");
-
-
-const randNum = Math.floor(Math.random() * questionList.length);
-
-question.innerText = questionList[randIndex]
-
